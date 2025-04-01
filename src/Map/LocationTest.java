2ddf7aaf60482fc915928a8edc59bb30254564c7
damia <dambibimbus@gmail.com>
2025-04-01 19:25:37 +0200

    Location test

---
+++ src/Map/LocationTest.java
@@ -0,0 +1,48 @@
+package Map;
+
+import Efects.Strenght;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationTest {
+    Location loc;
+    WorldMap world;
+
+    @BeforeEach
+    void setUp() {
+        world = new WorldMap();
+        world.initialize();
+        loc = world.getCurrentLoc();
+    }
+
+    @Test
+    void getName() {
+        assertEquals("Tavern", loc.getName());
+    }
+
+    @Test
+    void getGoToLocations() {
+        int[] ids = new int[]{5};
+        for (int i = 0; i < ids.length; i++) {
+            assertEquals(ids[i], loc.getGoToLocations()[i]);
+        }
+        world.goTo(5);
+        loc = world.getCurrentLoc();
+        ids = new int[]{1, 2, 3, 4, 6};
+        for (int i = 0; i < ids.length; i++) {
+            assertEquals(ids[i], loc.getGoToLocations()[i]);
+        }
+    }
+
+    @Test
+    void getId() {
+        assertEquals(1, loc.getId());
+    }
+
+    @Test
+    void apply() {
+        assertInstanceOf(Strenght.class, loc.apply());
+    }
+}
