94faf2b3760e1117f8f4fbfa47d7f251ea278a60
damia <dambibimbus@gmail.com>
2025-04-01 20:06:34 +0200

    testi commandu

---
+++ src/Command/AttackTest.java
@@ -0,0 +1,33 @@
+package Command;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AttackTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void execute() {
+    }
+
+    @Test
+    void apply() {
+    }
+
+    @Test
+    void endsTurn() {
+    }
+
+    @Test
+    void startInteraction() {
+    }
+
+    @Test
+    void energyCost() {
+    }
+}
---
+++ src/Command/GoToTest.java
@@ -0,0 +1,34 @@
+package Command;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoToTest {
+
+    @BeforeEach
+    void setUp() {
+        Command cmd = new GoTo();
+    }
+
+    @Test
+    void execute() {
+    }
+
+    @Test
+    void exit() {
+    }
+
+    @Test
+    void apply() {
+    }
+
+    @Test
+    void endsTurn() {
+    }
+
+    @Test
+    void energyCost() {
+    }
+}
