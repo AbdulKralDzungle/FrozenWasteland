536710a59ddb10d87fd455f143be15ca38dfec0c
damia <dambibimbus@gmail.com>
2025-04-01 18:01:07 +0200

    world test hotov

--- src/Map/WorldMapTest.java
+++ src/Map/WorldMapTest.java
@@ -6,20 +6,39 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorldMapTest {
+    private WorldMap wm;
 
     @BeforeEach
     void setUp() {
+        wm = new WorldMap();
+        wm.initialize();
     }
 
     @Test
     void getCurrentId() {
+        assertEquals(1, wm.getCurrentId());
+        wm.goTo(5);
+        assertEquals(5, wm.getCurrentId());
+        wm.goTo(-1);
+        assertEquals(5, wm.getCurrentId());
+        wm.goTo(10);
+        assertEquals(5, wm.getCurrentId());
+        wm.goTo(Integer.MAX_VALUE);
+        assertEquals(5, wm.getCurrentId());
     }
 
     @Test
     void getCurrentLoc() {
+        wm.goTo(5);
+        assertEquals(5, wm.getCurrentId());
+        wm.goTo(1);
+        assertEquals(1, wm.getCurrentId());
     }
 
     @Test
     void goTo() {
+        assertTrue(wm.goTo(5));
+        assertFalse(wm.goTo(-1));
+        assertFalse(wm.goTo(Integer.MAX_VALUE));
     }
 }
