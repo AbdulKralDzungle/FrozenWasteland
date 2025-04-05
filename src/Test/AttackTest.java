3ad11de86251bf2d42698c06efa5f94d2bdddf52
damia <dambibimbus@gmail.com>
2025-04-05 18:29:42 +0200

    attack command setup

--- src/Test/AttackTest.java
+++ src/Test/AttackTest.java
@@ -1,16 +1,31 @@
 package Test;
 
+import Command.Attack;
+import Command.Command;
+import Map.WorldMap;
+import Npcs.Enemes.Crawler;
+import Npcs.Npc;
+import Player.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AttackTest {
+    private Command cmd;
+    private WorldMap wm;
+    private Player p;
+    private Npc n;
 
     @BeforeEach
     void setUp() {
+        cmd = new Attack();
+        wm = new WorldMap();
+        p = new Player();
+        n = new Crawler();
     }
 
     @Test
     void execute() {
+        //cmd.execute(wm, )
     }
 
     @Test
